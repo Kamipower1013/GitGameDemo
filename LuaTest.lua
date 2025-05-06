853b26363e565cbe6c755e48c91b2f8e9774e31f
changzeliu <404683398@qq.com>
2025-05-06 13:48:05 +0800

    第一次提交测试

---
+++ LuaTest.lua
@@ -0,0 +1,20 @@
+print("111111")
+local var1=50
+
+local table1={}
+for i = 1, var1, 1 do
+    table.insert(table1,i)
+   if i==10 then
+      break
+   end
+
+    if i==25 then
+        break
+    end
+  
+end
+
+for i = 1, #table1, 1 do
+    print("pos:"..i.."value:"..table1[i])
+    
+end
