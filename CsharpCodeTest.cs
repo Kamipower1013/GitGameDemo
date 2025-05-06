c407c03ba191c3907c030d6d3857ab1d2ac9d624
changzeliu <404683398@qq.com>
2025-05-06 14:11:45 +0800

    第二次提交

---
+++ CsharpCodeTest.cs
@@ -0,0 +1,59 @@
+public partial class Player{
+
+private int ID;
+public string playerName;
+
+protected int curLevel;
+public Player(int ID ,string Name,int Level){
+    this.ID = ID;
+    this.playerName = Name;
+
+}
+
+
+
+
+
+}
+
+
+public  class BaseSkillUnit{}
+
+public class Player{} : BaseSkillUnit{}
+
+
+
+public partial class Player{
+    public List<>
+
+
+
+}
+
+
+
+
+public class PlayerList:using UnityEngine;
+
+public class CsharpCodeTest : MonoBehaviour {
+    
+
+    void Awake() {
+        
+    }
+    void Start(){
+
+
+    
+    }
+
+
+   void Update(){
+
+
+   }
+   void LateUpdate() {
+    
+   }
+
+}
